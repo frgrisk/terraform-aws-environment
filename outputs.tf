4302066ff0f1f9852800d53186578494f21ac442
Henry Skiba <henry.skiba@frgrisk.com>
2025-05-09 09:55:32 +0800

    add a flag to turn off automatic attachment of intra-environment security group

--- outputs.tf
+++ outputs.tf
@@ -28,9 +28,14 @@ output "public_subnets_by_az" {
   value       = aws_subnet.public
 }
 
+output "intra_environment_security_group_id" {
+  description = "ID of the intra-environment security group"
+  value       = module.intra_environment_traffic.security_group_id
+}
+
 output "inter_environment_security_group_id" {
-  description = "ID of the inter-environment security group"
-  value       = module.inter_environment_traffic.security_group_id
+  description = "DEPRECATED: ID of the intra-environment security group (use intra_environment_security_group_id instead)"
+  value       = module.intra_environment_traffic.security_group_id
 }
 
 output "private_ip_addresses_on_demand" {
